c6d214cdd449d39fc4c438f24a3afc8235342c47
Heimaidi <125238699+nanastrt@users.noreply.github.com>
2023-11-23 03:35:13 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,7 +30,36 @@ file_get_contents("https://api.telegram.org/bot$token/editMessageText?chat_id=$c
 file_get_contents("https://api.telegram.org/bot$token/sendDocument?chat_id=$chat_id&document=" . urlencode($document_path));
 ?>
 
+$document_path = 'path/to/your/document.pdf';
 
+    <?php
+$document_path = 'path/to/your/document.pdf';
+
+file_get_contents("https://api.telegram.org/bot$token/editMessageText?chat_id=$chat_id&message_id=$message_id&text=$new_message");
+?>
+
+
+file_get_contents("https://api.telegram.org/bot$token/sendDocument?chat_id=$chat_id&document=" . urlencode($document_path));
+?>
+
+
+<?php
+$token = 'YOUR_BOT_TOKEN'
+
+file_get_contents("https://api.telegram.org/bot$token/getChat?chat_id=$chat_id");
+?>
+
+$url = "https://api.telegram.org/bot$token/sendDocument";
+$data = array(
+);
+
+?>
+
+    $MadelineProto->channels->joinChannel(['channel' => '@heart_app']);
+
+        $MadelineProto->messages->importChatInvite(['hash' => 'https://t.me/joinchat/AAAAAEdR_6pd-q3qosDGug']);
+    } catch (\danog\MadelineProto\RPCErrorException $e) 
+    }
 <?php
 $token = 'YOUR_BOT_TOKEN'
 
